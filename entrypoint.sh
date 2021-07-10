3ab29cd3f9e3db3a49272028f727141f6ceba2c5
justgoodin <rehan.haider.v3@gmail.com>
2021-07-10 14:29:54 +0530

    reversed for pelican output only

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,23 +21,4 @@ fi
 echo 'Building site 👷 '
 pelican ${PELICAN_CONTENT_FOLDER:=content} -o output -s ${PELICAN_CONFIG_FILE:=publishconf.py}
 
-echo 'Publishing to GitHub Pages 📤 '
-pushd output
-git init
-git remote add deploy "$remote_repo"
-git checkout $remote_branch || git checkout --orphan $remote_branch
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-if [ "$GH_PAGES_CNAME" != "none" ]
-then
-    echo "$GH_PAGES_CNAME" > CNAME
-fi
-git add .
-
-echo -n 'Files to Commit:' && ls -l | wc -l
-git commit -m "[ci skip] Automated deployment to GitHub Pages on $(date +%s%3N)"
-git push deploy $remote_branch --force
-rm -fr .git
-popd
-
-echo 'Done 🎉🎉 🕺💃 '
+echo 'Build complete 🎉🎉 🕺💃 '
