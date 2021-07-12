fb4cd9b03eee5fa1f73aaa3cb9ac4a807c6dfd69
justgoodin <rehan.haider.v3@gmail.com>
2021-07-12 16:17:02 +0530

    Pelican Build

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,22 +6,15 @@ echo "REPO: $GITHUB_REPOSITORY"
 echo "ACTOR: $GITHUB_ACTOR"
 
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-remote_branch=${GH_PAGES_BRANCH:=gh-pages}
 
 echo 'Installing Python 🐍 Requirements  '
 pip install -r requirements.txt
 
-if [ -n "$PELICAN_THEME_FOLDER" ]; then
-    echo 'Installing Node Modules 🧰 '
-    pushd $PELICAN_THEME_FOLDER
-    npm install
-    popd
-fi
 
 echo 'Building site 👷 '
-pelican ${PELICAN_CONTENT_FOLDER:=content} -o output -s ${PELICAN_CONFIG_FILE:=publishconf.py}
+pelican ${PELICAN_CONTENT_FOLDER:=content} -o ${PELICAN_OUTPUT_FOLDER:=output} -s ${PELICAN_CONFIG_FILE:=publishconf.py}
 
-echo 'Running add-ons ❌ 👉🏽 ✔ '
+echo 'Running add-ons ➕➕ '
 python addons.py
 
 echo 'Build complete 🎉🎉 🕺💃 '
